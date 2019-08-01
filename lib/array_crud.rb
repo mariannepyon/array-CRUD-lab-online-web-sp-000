7166f79b74ba9abf3f1be30546b412f0af10ff95
Marianne Pyon <mariannepyon@gmail.com>
2019-08-01 18:59:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["Sarah", "Sally", "Dana", "Connie"]
-  array.pop("Connie")
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
