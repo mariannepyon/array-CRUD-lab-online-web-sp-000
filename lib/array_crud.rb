9c2445ddb8ff72f32a2ee8a0bfb213c8eb811885
Marianne Pyon <mariannepyon@gmail.com>
2019-08-01 18:56:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,7 +10,6 @@ def add_element_to_end_of_array(array, element)
   array = ["Sarah", "Sally", "Dana", "Connie"]
   element = "Maggie"
   array.push(element)
-  puts array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
