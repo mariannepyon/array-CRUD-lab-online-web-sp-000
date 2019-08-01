3e477815d2774afb402fc72bff12bf7a489c5793
Marianne Pyon <mariannepyon@gmail.com>
2019-08-01 18:52:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   create_an_array.push("Maggie")
-  puts add_element_to_end_of_array.inspect
+  puts create_an_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
