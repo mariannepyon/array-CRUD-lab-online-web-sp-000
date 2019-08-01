231d19815c411767d52c0d9e7c7881c63e2a5a61
Marianne Pyon <mariannepyon@gmail.com>
2019-08-01 18:54:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "Maggie"
+  array = ["Sarah", "Sally", "Dana", "Connie"]
+  element = "Maggie"
+  array.push(element)
   puts create_an_array.inspect
 end
 
